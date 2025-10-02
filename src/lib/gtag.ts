947ebd66e8275895baa42e4a75d23fbbfc1616b2
Evan Parra <eraphaelparra@gmail.com>
2025-10-02 21:03:00 +0000

    Ok, I'm wondering we can print some thing in the web consoel logs that c

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,10 +1,13 @@
+
 // src/lib/gtag.ts
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = (action: string, params: { [key: string]: any }) => {
   const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   if (typeof window.gtag !== 'function' || !GA_MEASUREMENT_ID) {
+    console.warn('[GTAG] gtag function not found or Measurement ID is missing. Event not sent.');
     return;
   }
+  console.log(`[GTAG Event] Action: ${action}, Params:`, params);
   window.gtag('event', action, params);
 };

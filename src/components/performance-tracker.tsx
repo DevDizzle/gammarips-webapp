8727fa707b2080769415248d96caaac6253f5771
Evan Parra <eraphaelparra@gmail.com>
2025-10-16 13:54:40 +0000

    Ok can we update tracked since 10/01/2025 to 10/16/2025? I updated the s

--- src/components/performance-tracker.tsx
+++ src/components/performance-tracker.tsx
@@ -34,7 +34,7 @@ export async function PerformanceTracker() {
                     </CardTitle>
                 </div>
                  <CardDescription className="max-w-2xl mx-auto">
-                    This data reflects the historical performance of our AI data pipeline, tracked since 10/01/2025 from the date of identification to the standard expiry. All data is for informational purposes and does not guarantee future results.
+                    This data reflects the historical performance of our AI data pipeline, tracked since 10/16/2025 from the date of identification to the standard expiry. All data is for informational purposes and does not guarantee future results.
                 </CardDescription>
             </CardHeader>
             <CardContent>
@@ -86,6 +86,7 @@ export function PerformanceTrackerSkeleton() {
 }
 
 export default PerformanceTracker;
+
 
 
 

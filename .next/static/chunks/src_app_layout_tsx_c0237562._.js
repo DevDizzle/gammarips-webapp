230d63e76fdd3175d6341e57692b21419d9270d2
Evan Parra <eraphaelparra@gmail.com>
2025-10-24 18:51:31 +0000

    Ok, you completed wiped out our .env that had all of our secrets...I tri

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -2,7 +2,12 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/node_modules_f1dfca5b._.js",
+  "static/chunks/node_modules_next_12ec1765._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/d9ef2_@firebase_auth_dist_esm2017_470e6e8f._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+  "static/chunks/node_modules_zod_v3_209a51fb._.js",
+  "static/chunks/node_modules_2ab1668d._.js",
   "static/chunks/src_7503b6f0._.js"
 ],
     source: "dynamic"

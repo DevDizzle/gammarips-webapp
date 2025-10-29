37393333a650c93a9b4fead7183e71ee319238cc
Evan Parra <eraphaelparra@gmail.com>
2025-10-29 17:47:49 +0000

    I noticed we moved the Historical Perfromance to landing which is gooc b

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,11 +3,15 @@ import { Suspense } from "react";
 import TodaysWinners from "@/app/dashboard/todays-winners";
 import DashboardPageClient from "./dashboard-client";
 import { IndustryExplorer, IndustryExplorerSkeleton } from "./industry-explorer";
+import PerformanceTracker, { PerformanceTrackerSkeleton } from "@/components/performance-tracker";
 
 function DashboardContent() {
     return (
         <div className="space-y-8">
             <TodaysWinners />
+            <Suspense fallback={<PerformanceTrackerSkeleton />}>
+                <PerformanceTracker />
+            </Suspense>
             <Suspense fallback={<IndustryExplorerSkeleton />}>
               <IndustryExplorer />
             </Suspense>

813776706e4dc0d95f4d201d779c5c4fb1e854dc
Evan Parra <eraphaelparra@gmail.com>
2025-11-30 22:06:55 +0000

    Ok she hates the font, I like it. But if we can ompress her we have a wi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Plus Jakarta Sans', 'sans-serif'],
+        headline: ['Space Grotesk', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {

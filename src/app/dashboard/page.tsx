b8e48d73e9c26f839bfc6271f2736acb438ed01b
Evan Parra <eraphaelparra@gmail.com>
2025-12-22 11:08:46 +0000

    No, we want to hid my wathclist for now, comment it out. We'll circle ba

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default async function DashboardPage() {
       </section>
 
       {/* Main Grid Layout */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-start">
         
         {/* Main Column: 66% */}
         <div className="lg:col-span-2 space-y-6">
@@ -32,7 +32,7 @@ export default async function DashboardPage() {
         <div className="lg:col-span-1 space-y-6">
             
             {/* Watchlist Widget (Personalized/Gated) */}
-            <WatchlistWidget />
+            {/* <WatchlistWidget /> */}
 
             {/* News Feed (Contextual) */}
             <NewsFeedWidget />
